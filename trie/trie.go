1a1c12242cca705302a9959848ef02881502401f
Thomas <thomaslee9474@gmail.com>
2019-11-22 12:38:51 -0800

    [add] is empty

--- trie/trie.go
+++ trie/trie.go
@@ -2,7 +2,22 @@ package trie
 
 import "github.com/ThomasLee94/autosuggest/node"
 
+// ******
+// Struct
+// ******
 type Trie struct {
 	root *node.Node
 	size int
+}
+
+// ******************
+// Trie class methods
+// ******************
+func (trie *Trie) IsEmpty() bool {
+	// Return True if this prefix tree is empty (contains no strings).
+	if trie.size == 0 {
+		return true
+	}
+
+	return false
 }
