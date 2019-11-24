461931527023d65f99d08bc709677315ef119b49
Thomas <thomaslee9474@gmail.com>
2019-11-24 00:18:22 -0800

    [add] node test

--- test/node_test.go
+++ test/node_test.go
@@ -39,25 +39,15 @@ func TestNodeChildMethods(t *testing.T) {
 	nodeStructB := node.Node{}
 	nodeStructB.Character = nodeCharB
 
-	// test add children
-	nodeSructA.AddChildren(nodeCharB, nodeStructB)
+	// test add & get children
+	nodeStructA.AddChildren(nodeCharB, nodeStructB)
+	if !nodeStructA.HasChildren(nodeCharB) {
+		t.Errorf("Node HasChildren is %d; want %d", nodeStructA.HasChildren(nodeCharB), true)
+	}
 
-	# Create node 'A' and verify it does not have any children
-	node_A = PrefixTreeNode('A')
-	assert node_A.num_children() == 0
-	assert node_A.has_child('B') is False
-	# Verify getting child from node 'A' raises error
-	with self.assertRaises(ValueError):
-			node_A.get_child('B')
-	# Create node 'B' and add it as child to node 'A'
-	node_B = PrefixTreeNode('B')
-	node_A.add_child('B', node_B)
-	# Verify node 'A' has node 'B' as child
-	assert node_A.num_children() == 1
-	assert node_A.has_child('B') is True
-	child_node = node_A.get_child('B')
-	assert child_node is node_B
-	# Verify adding node 'B' as child to node 'A' again raises error
-	with self.assertRaises(ValueError):
-			node_A.add_child('B', node_B)
+	// test get children
+	childNode, _ := nodeStructA.GetChildren(nodeCharB)
+	if childNode.Character != nodeCharB {
+		t.Errorf("Node GChildren is %d; want %d", childNode.Character, nodeCharB)
+	}
 }
