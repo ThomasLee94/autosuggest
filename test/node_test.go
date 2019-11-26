f82103cd657c62987a0626ef5762130eabfa728c
Thomas <thomaslee9474@gmail.com>
2019-11-26 10:51:44 -0800

    [WIP] test refactor with assert package

--- test/node_test.go
+++ test/node_test.go
@@ -4,44 +4,42 @@ import (
 	"testing"
 
 	"github.com/ThomasLee94/autosuggest/node"
+	"gotest.tools/assert"
 )
 
-func TestTableNodeStruct(t *testing.T) {
-	var tests = []struct {
-		input    string
-		expected string
-		error    string
-	}{
-		{"A", "A", "NewNode('A') is {}; want {}"},
-		{-1, 1},
-		{0, 2},
-		{-5, -3},
-		{99999, 100001},
-	}
+// func TestTableNodeStruct(t *testing.T) {
+// 	var tests = []struct {
+// 		input    string
+// 		expected string
+// 		error    string
+// 	}{
+// 		{"A", "A", "NewNode('A') is {}; want {}"},
+// 		{-1, 1},
+// 		{0, 2},
+// 		{-5, -3},
+// 		{99999, 100001},
+// 	}
 
-	for _, test := range tests {
-		if output := node.NewNode(test.input); output != test.expected {
-			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
-		}
-	}
-}
+// 	for _, test := range tests {
+// 		if output := node.NewNode(test.input); output != test.expected {
+// 			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+// 		}
+// 	}
+// }
 func TestNodeStructAttributes(t *testing.T) {
 	// init node struct
 	nodeChar := "A"
 	nodeStruct := node.NewNode(nodeChar)
 
 	// node.Character == "A"
-	if nodeStruct.Character != "A" {
-		t.Errorf("Node Init('A') is %s; want %s", nodeStruct.Character, nodeChar)
-	}
+	assert.Equal(t, nodeStruct.Character, nodeChar)
+
 	// node.childred == empty dict
-	if len(nodeStruct.Children) != 0 {
-		t.Errorf("Length of node is = %d; want %d", len(nodeStruct.Children), 0)
-	}
+	assert.Equal(t, len(nodeStruct.Children), 0)
+
 	// node.terminal == false
-	if nodeStruct.Terminal != false {
-		t.Errorf("Node terminal is %t; want %t", nodeStruct.Terminal, false)
-	}
+	assert.Equal(t, nodeStruct.Terminal, false)
+
 }
 
 func TestNodeChildMethods(t *testing.T) {
