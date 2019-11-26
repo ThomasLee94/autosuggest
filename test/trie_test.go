6078b9c7417159e6f80a541018967128972a5e48
Thomas <thomaslee9474@gmail.com>
2019-11-26 11:14:29 -0800

    [refactor] test insert

--- test/trie_test.go
+++ test/trie_test.go
@@ -53,49 +53,22 @@ func TestInsert(t *testing.T) {
 	trieObj.Insert(prefix)
 
 	// test root attributes
-	if trieObj.Root.Character != "" {
-		t.Errorf("Size of Trie is %s; want %s", trieObj.Root.Character, "")
-	}
-
-	if trieObj.Root.IsTerminal() {
-		t.Errorf("Root node of trie is %t; want %t", trieObj.Root.IsTerminal(), false)
-	}
-
-	if len(trieObj.Root.Children) != 1 {
-		t.Errorf("Root node has %d children; want %d", trieObj.Root.Children, 1)
-	}
+	assert.Equal(t, trieObj.Root.Character, "")
+	assert.Equal(t, trieObj.Root.IsTerminal(), false)
+	assert.Equal(t, len(trieObj.Root.Children), 1)
 
 	// test 'A' node
 	childNodeA, _ := trieObj.Root.GetChildren("A")
-	if childNodeA.Character != "" {
-		t.Errorf("Root node does not have a child containing prefex %s", "A")
-	}
-
-	if childNodeA.IsTerminal() {
-		t.Errorf("Root node of trie is %t; want %t", childNodeA.IsTerminal(), false)
-	}
-
-	if len(childNodeA.Children) != 1 {
-		t.Errorf("node A has %d children; want %d", childNodeA.Children, 1)
-	}
-
-	if !childNodeA.HasChildren("B") {
-		t.Errorf("node A has %d children; want %d", childNodeA.Children, 1)
-	}
+	assert.Equal(t, childNodeA.Character, "")
+	assert.Equal(t, childNodeA.IsTerminal(), false)
+	assert.Equal(t, len(childNodeA.Children), 1)
+	assert.Equal(t, childNodeA.HasChildren("B"), true)
 
 	// test 'B' node
 	childNodeB, _ := childNodeA.GetChildren("B")
-	if childNodeB.Character != "B" {
-		t.Errorf("node A does not have a child containing prefex %s", "B")
-	}
-
-	if !childNodeB.IsTerminal() {
-		t.Errorf("Root node of trie is %t; want %t", childNodeA.IsTerminal(), true)
-	}
-
-	if len(childNodeB.Children) != 0 {
-		t.Errorf("node A has %d children; want %d", len(childNodeB.Children), 0)
-	}
+	assert.Equal(t, childNodeB.Character, "B")
+	assert.Equal(t, childNodeB.IsTerminal(), true)
+	assert.Equal(t, len(childNodeB.Children), 0)
 
 }
 
