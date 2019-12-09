f1726a1a320c2a13c13bab6f2e2438cbc38e6a5e
Thomas <thomaslee9474@gmail.com>
2019-12-08 20:48:40 -0800

    [update] root cmd to insert chars from terminal into trie

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,31 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"bufio"
+	"os"
+
+	"github.com/ThomasLee94/autosuggest/trie"
+	"github.com/spf13/cobra"
+)
 
 //RootCmd for autosuggest
 var RootCmd = &cobra.Command{
 	Use:   "",
 	Short: "Listening to all commands for auto-suggestions!",
+	Run: func(cmd *cobra.Command, args []string) {
+		// create reader to read from standard input
+		reader := bufio.NewReader(os.Stdin)
+		// saves chars from terminal input
+		chars, _ := reader.ReadString('\n')
+		// insert strings into trie
+		trie := trie.NewTrie()
+		for char := range chars {
+			trie.Insert(string(char))
+		}
+	},
+}
+
+//	Adds show command to root command
+func init() {
+	RootCmd.AddCommand(RootCmd)
 }
