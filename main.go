bc83483c9bc6f3a9794bd7ddc1d277002ed13007
Thomas <thomaslee9474@gmail.com>
2019-12-08 18:53:20 -0800

    [fix] main to execute cobra

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	asdf "github.com/ThomasLee94/autosuggest/trie"
+	"github.com/ThomasLee94/autosuggest/cmd"
 )
 
 func main() {
-	fmt.Println("ehl")
-
-	trieObj := asdf.NewTrie()
-	prefix1 := "ABC"
-	prefix2 := "ABE"
-	prefix3 := "A"
-	prefix4 := "EFG"
-
-	trieObj.Insert(prefix1)
-	trieObj.Insert(prefix2)
-	trieObj.Insert(prefix3)
-	trieObj.Insert(prefix4)
-
-	childNodeA, _ := trieObj.Root.GetChildren("A")
-	childNodeB, _ := childNodeA.GetChildren("B")
-	// childNodeE, _ := childNodeB.GetChildren("E")
-
+	cmd.Execute()
 }
