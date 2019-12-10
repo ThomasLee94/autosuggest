6a8a10c5ef6f929a731a653bf0ca4ae5f9d08d75
Thomas <thomaslee9474@gmail.com>
2019-12-09 17:47:55 -0800

    [WIP] streaming characters instead of trie object

--- cmd/root.go
+++ cmd/root.go
@@ -17,26 +17,25 @@ var RootCmd = &cobra.Command{
 
 		trieObj := trie.NewTrie()
 		// trie object channel to allow for grayed out text
-		trieChannel := make(chan trie.Trie)
+		stringChannel := make(chan string)
+		// create reader to read from standard input
+		reader := bufio.NewReader(os.Stdin)
 
 		// goroutine to build trie with user input being streamed in
 		// stream characters as it is being typed
 		func()  {
-			// create reader to read from standard input
-			reader := bufio.NewReader(os.Stdin)
 			// saves chars from terminal input
 			chars, _ := reader.ReadString('\n')
 			// insert strings into trie
 			for char := range chars {
 				trieObj.Insert(string(char))
+				stringChannel <- string(char)
 			}
-
-			trieChannel <- *trieObj
 		}()
 
 		// goroutine to show grayed out text
 		func() []string{
-			return trieObj.Complete(string(trieChannel))
+			return trieObj.Complete(stringChannel)
 		}()
 
 		//input <- trieChannel
