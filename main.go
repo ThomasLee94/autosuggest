54b746f8468b1f8198fa8a30b1f1a920e9e16fd8
Thomas <thomaslee9474@gmail.com>
2019-12-09 21:44:15 -0800

    [add] boltdb; so different cobra flags can access built trie

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"github.com/ThomasLee94/autosuggest/cmd"
+	"github.com/boltdb/bolt"
+	"log"
 )
 
 func main() {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	cmd.RootCmd.Execute()
 }
