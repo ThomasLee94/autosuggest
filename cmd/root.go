6f100b931b59041fc160e755c3b8c18949d2a092
Thomas <thomaslee9474@gmail.com>
2019-12-09 20:32:08 -0800

    [refactor] a root command is necessary with no workaround, will need a ground up redesign for fish-like autosuggestions

--- cmd/root.go
+++ cmd/root.go
@@ -10,39 +10,7 @@ import (
 
 //RootCmd for autosuggest
 var RootCmd = &cobra.Command{
-	Use:   "",
-	Short: "Listening to all commands for auto-suggestions!",
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: concurrently insert user input & showing completions text
-
-		trieObj := trie.NewTrie()
-		// trie object channel to allow for grayed out text
-		stringChannel := make(chan string)
-		// create reader to read from standard input
-		reader := bufio.NewReader(os.Stdin)
-
-		// goroutine to build trie with user input being streamed in
-		// stream characters as it is being typed
-		func()  {
-			// saves chars from terminal input
-			chars, _ := reader.ReadString('\n')
-			// insert strings into trie
-			for char := range chars {
-				trieObj.Insert(string(char))
-				stringChannel <- string(char)
-			}
-		}()
-
-		// goroutine to show grayed out text
-		func() []string{
-			return trieObj.Complete(stringChannel)
-		}()
-
-		//input <- trieChannel
-	},
+	Use:   "as",
+	Short: "A CLI tool to add auto suggestions for your terminal!",
 }
 
-//	Adds show command to root command
-func init() {
-	RootCmd.AddCommand(RootCmd)
-}
